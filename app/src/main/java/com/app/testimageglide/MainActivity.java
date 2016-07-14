7f2c733949aea345d3646d7589db010940363de9
John Elmer Semaya <elmerrxdoo@gmail.com>
2016-07-14 10:05:10 +0700

    Initial commit

---
+++ app/src/main/java/com/app/testimageglide/MainActivity.java
@@ -0,0 +1,19 @@
+package com.app.testimageglide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageView = (ImageView) findViewById(R.id.image1);
+        Glide.with(this).load("http://goo.gl/gEgYUd").into(imageView);
+    }
+}
